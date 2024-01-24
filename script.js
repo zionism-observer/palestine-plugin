8cc3ab9afe3a895deb945c13ea2282c53c2765aa
cyberDaffodil <cyberDaffodil@proton.me>
2024-01-24 00:49:56 -0500

    prototype

---
+++ script.js
@@ -0,0 +1,32 @@
+// TODO: Switch to Typescript
+const getActiveTab = () => {
+  return new Promise(function (resolve, reject) {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+      if (tabs != null && 0 < tabs.length) {
+        resolve(tabs[0]);
+      } else {
+        reject();
+      }
+    });
+  });
+};
+
+const createTab = (options) => {
+  return new Promise((resolve) => {
+    chrome.tabs.create(options, resolve);
+  });
+};
+
+const openArchive = async () => {
+  const activeTab = await getActiveTab();
+  console.log("activeTab", activeTab);
+
+  const currentUrl = activeTab.url;
+  await createTab({
+    url: "https://zionism.observer/archive?source=" + currentUrl,
+  });
+};
+
+document
+  .getElementById("redirectButton")
+  .addEventListener("click", openArchive);
